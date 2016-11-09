801484ab2cff560d09bc29cd95c7bf4e363102bd
cat kutay <cat.kutay@gmail.com>
2016-11-09 15:28:54 +1100

    Revert "Remove other branch"
    This reverts commit ff120fa3de910ff41f947df83bdc67c79035b3c1.

---
+++ HediehFatimaOmasa/Assets/Scripts/AssetManagerBridge.cs
@@ -0,0 +1,14 @@
+﻿using AssetPackage;
+using UnityEngine;
+using ILogger = GAIPS.Rage.ILogger;
+
+namespace Assets.Scripts
+{
+	public class AssetManagerBridge : IBridge, ILogger
+	{
+		public void Log(object msg)
+		{
+			Debug.Log(msg);
+		}
+	}
+}
