2a1004f59fb3d47949c5da6ab7a69c23ab26adb2
cat kutay <cat.kutay@gmail.com>
2016-11-09 16:00:48 +1100

    Added Sarah and Libsync

---
+++ Third Demo/Assets/AnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown("1"))
+			anim.SetInteger("animation_type",1);
+		if(Input.GetKeyDown("2"))
+			anim.SetInteger("animation_type",2);
+		if(Input.GetKeyDown("3"))
+			anim.SetInteger("animation_type",3);
+	
+	}
+}
